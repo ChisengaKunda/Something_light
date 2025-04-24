108ac4bb4e115911547da73b00a683dc2d117240
Ccleveland09ner <kazundachawana@gmail.com>
2025-04-23 22:51:58 -0500

    new frontend loading

--- Back/db/db_handler.py
+++ Back/db/db_handler.py
@@ -5,9 +5,9 @@ from typing import List
 DB_FILE = Path("db/patient_db.json")
 
 def read_db() -> List[dict]:
-    if DB_FILE.exists():
-        return json.loads(DB_FILE.read_text())
-    return []
+    if not DB_FILE.exists() or DB_FILE.read_text().strip() == "":
+        DB_FILE.write_text("[]")
+    return json.loads(DB_FILE.read_text())
 
 def write_db(data: List[dict]) -> None:
     DB_FILE.write_text(json.dumps(data, indent=2))
